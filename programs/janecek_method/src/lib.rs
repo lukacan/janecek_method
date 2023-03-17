0e52058aa2a336c959d2d70f829df84c5bb3ae7c
lukacan <andrej.lukacovic98@gmail.com>
2023-03-17 12:11:22 +0100

    create_party,create_voter

--- programs/janecek_method/src/lib.rs
+++ programs/janecek_method/src/lib.rs
@@ -1,21 +1,40 @@
 use anchor_lang::prelude::*;
+use solana_program::{pubkey, pubkey::Pubkey};
 
 declare_id!("Fnambs3f1XXoMmAVc94bf8t6JDAxmVkXz85XU4v2edph");
+pub const ADMIN: Pubkey = pubkey!("G6ScTg7oSQPoKv6WtikLJeFgnE85TJBCPFAaiha7qbzJ");
 
 #[program]
 pub mod janecek_method {
     use super::*;
     pub fn create_party(ctx: Context<CreateParty>, name: String)-> Result<()>{
+        if name.chars().count() > 32
+        {
+            return Err(ErrorCode::NameTooLong.into())
+        }
+
         let party: &mut Account<Party> = &mut ctx.accounts.party;
         let author: &mut Signer = &mut ctx.accounts.author;
         let clock: Clock = Clock::get().unwrap();
         
+        party.bump = *ctx.bumps.get("party").unwrap();
         party.author = *author.key;
         party.created = clock.unix_timestamp;
         party.name = name;
         party.votes = 0;
+        
         Ok(())
     }
+    pub fn create_voter(ctx: Context<CreateVoter>)->Result<()>{
+        let voter: &mut Account<Voter> = &mut ctx.accounts.voter;
+        voter.num_votes = 0;
+        voter.can_vote = true;
+        Ok(())
+    }
+    // pub fn vote(ctx: Context<Vote>)->Result<()>{
+
+    // }
+
 }
 #[account]
 pub struct Party{
@@ -25,24 +44,59 @@ pub struct Party{
     pub votes: i64, 
     pub bump: u8,
 }
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4;
+const MAX_NAME_LENGTH: usize = 32 * 4;
+const NUM_VOTES_LENGTH: usize = 8;
+const BUMP_LENGTH: usize = 1;
+
+impl Party {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH
+        + TIMESTAMP_LENGTH
+        + STRING_LENGTH_PREFIX + MAX_NAME_LENGTH
+        + NUM_VOTES_LENGTH
+        + BUMP_LENGTH;
+}
+
 #[account]
 pub struct Voter{
-    pub who: Pubkey,
     pub num_votes: i8,
-}
-#[derive(Accounts)]
-pub struct Vote<'info> {
-    pub tweet: Account<'info, Voter>,
-    pub author: Signer<'info>,
-    pub system_program: Program<'info, System>,
+    pub can_vote: bool,
 }
 
-
 #[derive(Accounts)]
+#[instruction(name: String)]
 pub struct CreateParty<'info> {
     #[account(mut)]
     pub author: Signer<'info>,
-    #[account(init, payer = author, space = 200, seeds = ["name2".as_bytes()],bump)]
+    #[account(init, payer = author, space = Party::LEN, seeds = [name.as_bytes()],bump)]
     pub party: Account<'info, Party>,
     pub system_program: Program<'info, System>,
+}
+#[derive(Accounts)]
+pub struct CreateVoter<'info>{
+    #[account(mut,address=ADMIN @ ErrorCode::PermissionDenied)]
+    pub admin: Signer<'info>,
+    #[account(init, payer = admin, space = 200)]
+    pub voter: Account<'info,Voter>,
+    pub system_program: Program<'info, System>,
+
+}
+
+// #[derive(Accounts)]
+// pub struct Vote<'info>{
+//     #[account(mut,address=MY_PUBKEY @ )]
+// }
+
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Party Name Too Long")]
+    NameTooLong,
+    #[msg("Only admin can add new Voters")]
+    PermissionDenied,
 }
--- tests/janecek_method.ts
+++ tests/janecek_method.ts
@@ -1,33 +1,123 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { AnchorError,Program } from "@project-serum/anchor";
 import { JanecekMethod } from "../target/types/janecek_method";
-import { PublicKey } from '@solana/web3.js'
+import { PublicKey, Keypair } from '@solana/web3.js'
+import * as assert from "assert";
 
 describe("janecek_method", () => {
-  // Configure the client to use the local cluster.
+  let admin_secretKey = Uint8Array.from([206,146,51,36,247,155,237,189,223,
+    244,222,196,51,107,85,193,76,22,132,33,97,251,158,85,93,55,197,87,
+    122,101,145,176,224,68,42,180,144,146,5,12,50,29,187,121,160,8,189,
+    245,198,249,155,243,106,173,77,196,113,211,235,27,128,134,150,195]);
+
+  let admin_keypair = Keypair.fromSecretKey(admin_secretKey);
+  
+
+
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.JanecekMethod as Program<JanecekMethod>;
 
-  it('can create party', async () => {
-    // Before sending the transaction to the blockchain.
-    const party = anchor.web3.Keypair.generate();
-    const [userStatsPDA,number] = await PublicKey.findProgramAddress(
+
+  
+  it("Admin add funds",async () =>{
+    const signature = await program.provider.connection.requestAirdrop(admin_keypair.publicKey, 1000000000);
+    await program.provider.connection.confirmTransaction(signature);
+  })
+
+
+  it('Create Party', async () => {
+    
+    // create random user and airdrop
+    const ferko_mrkvicka = anchor.web3.Keypair.generate();
+    const signature = await program.provider.connection.requestAirdrop(ferko_mrkvicka.publicKey, 1000000000);
+    await program.provider.connection.confirmTransaction(signature);
+    
+    
+    // derive pda from party name
+    const [pda,bump] = await PublicKey.findProgramAddress(
       [
-        anchor.utils.bytes.utf8.encode('name2'),
+        anchor.utils.bytes.utf8.encode('political aprty'),
       ],
       program.programId
-    )    
-    console.log(userStatsPDA)
-    await program.rpc.createParty('SMER', {
-        accounts: {
-            author: program.provider.wallet.publicKey,
-            party: userStatsPDA,
-            systemProgram: anchor.web3.SystemProgram.programId,
-        }
-    });
+    )
 
-    //const partyAccount = await program.account.party.fetch(party.publicKey);
-    //console.log(partyAccount);
+    // check if party already exists, if dont, create
+    const info = await program.provider.connection.getAccountInfo(pda);
+    if(info != null)
+    {
+      console.log("Already exists")
+    }else
+    {
+      try {
+        await program.rpc.createParty('political aprty', {
+          accounts: {
+              author: ferko_mrkvicka.publicKey,
+              party: pda,
+              systemProgram: anchor.web3.SystemProgram.programId,
+          },signers:[
+            ferko_mrkvicka
+          ]
+      }); 
+      } catch (error) {
+      }
+    }
   });
+  it("Add Voter", async () =>{
+    const new_voter = anchor.web3.Keypair.generate();
+
+    const info = await program.provider.connection.getAccountInfo(new_voter.publicKey);
+    if(info != null)
+    {
+      console.log("PublicKey already registered as user")
+    }else
+    {
+      await program.rpc.createVoter({
+        accounts: {
+            admin: admin_keypair.publicKey,
+            voter: new_voter.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
+        },signers:[
+          admin_keypair,new_voter
+        ]
+      });
+    }
+
+  });
+
+  it("Add Voter with no admin rights", async () =>{
+    const new_voter = anchor.web3.Keypair.generate();
+    const new_admin = anchor.web3.Keypair.generate();
+
+    const signature = await program.provider.connection.requestAirdrop(new_admin.publicKey, 1000000000);
+    await program.provider.connection.confirmTransaction(signature);
+
+
+    const info = await program.provider.connection.getAccountInfo(new_voter.publicKey);
+    if(info != null)
+    {
+      console.log("PublicKey already registered as user")
+    }else
+    {
+      try {
+        await program.rpc.createVoter({
+          accounts: {
+              admin: new_admin.publicKey,
+              voter: new_voter.publicKey,
+              systemProgram: anchor.web3.SystemProgram.programId,
+          },signers:[
+            new_admin,new_voter
+          ]
+        });
+      } catch (error) {
+        assert.ok(error instanceof AnchorError);
+        assert.equal((error as AnchorError)
+        .error.errorCode.code, 'PermissionDenied');
+      }
+
+    }
+
+  });
+
+
 });
